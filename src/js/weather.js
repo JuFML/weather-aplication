a609231b9a97a8c8a2229fafadd47760e44c78b6
Juliana Fernandez <julianafernandezlima@hotmail.com>
2021-11-01 20:55:39 -0300

    Adding try/catch in FetchAPI

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,17 +1,25 @@
 const APIKey = 'Q9kpfRrlyQHcGjTAiLrQjtobDRMNJTU3'
 
 const getCityData = async (city) => {
-    const urlCityData = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${city}`
-    const response = await fetch(urlCityData)
-    const [cityData] = await response.json()
-    return cityData
+    try {
+        const urlCityData = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${city}`
+        const response = await fetch(urlCityData)
+        const [cityData] = await response.json()
+        return cityData
+    } catch (error) {
+        alert(`${error.name}: ${error.message}`)
+    }
 }
 
 const getCityWeather = async (cityKey) => {
-    const {Key, LocalizedName} = await getCityData(cityKey)
-    const urlCityWeather = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
-    const response = await fetch(urlCityWeather)
-    const [cityWeather] = await response.json()
-    return cityWeather
+    try {
+        const {Key, LocalizedName} = await getCityData(cityKey)
+        const urlCityWeather = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+        const response = await fetch(urlCityWeather)
+        const [cityWeather] = await response.json()
+        return cityWeather
+    } catch (error) {
+        alert(`${error.name}: ${error.message}`)
+    }
 }
 
